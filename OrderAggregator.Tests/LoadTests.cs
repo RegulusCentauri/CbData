31ad524148471636d7e6d981ab1cb5d7d222d454
jarolvik <jarolvik@fel.cvut.cz>
2024-09-28 22:39:01 +0200

    Adding LoadTest for handling a high amount of different ProductIds

--- OrderAggregator.Tests/LoadTests.cs
+++ OrderAggregator.Tests/LoadTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging.Abstractions;
+using System.Diagnostics;
 using System.Net.Http.Json;
 
 namespace OrderAggregator.Tests
@@ -16,7 +17,7 @@ namespace OrderAggregator.Tests
 
         [Fact]
         //Upgrade suggestion: Best would be to use HttpClient to call the PostOrders() directly, but that's getting blocked through localhost (some remote deployment would be necessary to run it)
-        public async Task LoadTest() {
+        public async Task LoadTest_RequestNumber_Local() {
             int totalRequests = 100; // Number of simulated requests
             var tasks = new List<Task>();
 
@@ -58,7 +59,7 @@ namespace OrderAggregator.Tests
 
 
         [Fact]
-        public async Task LoadTest_HttpClient() {
+        public async Task LoadTest_RequestNumber_HttpClient() {
             int totalRequests = 100;
             var tasks = new List<Task<HttpResponseMessage>>();
 
@@ -87,6 +88,58 @@ namespace OrderAggregator.Tests
 
             var response = await _httpClient.PostAsJsonAsync("/api/orders/PostOrders", orders);
             return response;
+        }
+
+        // In theory, only one Assert per test should be present, as it should test just ONE thing, but with this setup,
+        // not using the previous asserts could lead to a passed test, but with invalid steps
+        [Fact]
+        public async Task LoadTest_ProductIdCount_Local() {
+            int totalRequests = 700; // Number of simulated ProductIds
+            var tasks = new List<Task>();
+
+            var orderCache = new OrderCache();
+            var logger = NullLogger<OrdersController>.Instance;
+
+            var controller = new OrdersController(orderCache, logger);
+
+            //Timer setup
+            var timer = new Stopwatch();
+            timer.Start();
+
+            for(int i = 0 ; i < totalRequests ; i++) {
+                var index = i.ToString(); // Capture the current value of i, otherwise the Task.Run uses the same "i" value for each run
+
+                tasks.Add(Task.Run(() => {
+                    // Prepare orders
+                    var orders = new List<Order>
+                    {
+                        new Order
+                        {
+                            ProductId = index,
+                            Quantity = 1
+                        }
+                    };
+
+                    var result = controller.HandleIncomingOrders(orders);
+
+                    // Assert that the result is Accepted
+                    Assert.IsType<AcceptedResult>(result);
+                }));
+            }
+
+            // Wait for all tasks to complete
+            await Task.WhenAll(tasks);
+
+            //Timer stop
+            timer.Stop();
+            TimeSpan timeTaken = timer.Elapsed; //Time it took to process all the orders using the current solution
+
+            var aggregatedOrders = orderCache.GetAggregatedOrders();
+
+            // Verify that total order nr is correct
+            int totalOrders = aggregatedOrders.Count;
+            Assert.Equal(totalRequests, totalOrders);
+            Assert.True(timeTaken < TimeSpan.FromSeconds(1), $"Time taken to take {totalRequests} was too long ({timeTaken})");
         }
     }
 }
